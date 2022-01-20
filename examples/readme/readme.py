4dda9d4577db2b6b42354214c5e401d767e7cc6a
kakko3 <markku.kakko@hotmail.com>
2022-01-20 20:39:49 +0200

    Feature: Better readme

---
+++ examples/readme/readme.py
@@ -0,0 +1,25 @@
+from magiqt.application import Application
+from magiqt.field.fields import IntegerField, FloatField, DropDown, StringField
+from magiqt.field.range import ListRange
+from magiqt.layout_manager.form import Form
+
+
+class OperationSystem(Form):
+    name = StringField("OS name")
+    major = IntegerField("Major version")
+    minor = FloatField("Minor version")
+
+
+class MobilePhone(Form):
+    maker = DropDown("Dropdown", range=ListRange(["Nokia", "Sony", "Samsung"]))
+    mass = FloatField("Mass")
+    battery_capacity = FloatField("Battery capacity")
+    operation_system = OperationSystem("Operation system")
+
+
+if __name__ == "__main__":
+    APP = Application()
+    WIDGET = MobilePhone.new_tree("MobilePhone")
+    WIDGET.set_window_title("Test GUI")
+    WIDGET.show()
+    APP.exec()
